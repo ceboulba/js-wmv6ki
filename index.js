54a6728f4ede108d6edfd1794b5b508a92d29690
ceboulba <antoine@archipicture.fr>
2020-04-16 15:12:15 +0200

    1 file modified

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 import './style.css';
 
 import './pano2vr_player.js'
-import './pano2vr_player.js'
+import './skin.js'
+import './webvr/three.min.js'
+import './webvr/three.min.js'
+
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+// const appDiv = document.getElementById('app');
+// appDiv.innerHTML = `<h1>JS Starter</h1>`;
