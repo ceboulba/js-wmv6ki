447e71b59b8c08e32b7c8a82c0ef92cdf750c68d
ceboulba <antoine@archipicture.fr>
2020-04-17 00:26:04 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,35 +1,29 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
 
- import './pano2vr_player.js'
- import {pano2vrSkin} from './skin.js'
-import '.webvr/three.min.js'
-//  import * as Three from './webvr/three.min.js'
- import './webvr/webvr-polyfill.min.js'
+import ".webvr/three.min.js";
+import "./webvr/webvr-polyfill.min.js";
+import "./pano2vr_player.js";
+import { pano2vrSkin } from "./skin.js";
 
-const container = document.querySelector('#container')
+const container = document.querySelector("#container");
 
 // import * as pano2vrSkin from './skin.js'
 // import './webvr/three.min.js'
 
+// const pano=new pano2vrPlayer("container");
 
+// const skin=new pano2vrSkin(pano);
 
-			// const pano=new pano2vrPlayer("container");
+// create the panorama player with the container
+const pano = new pano2vrPlayer("container");
+// add the skin object
+const skin = new pano2vrSkin(pano);
+// load the configuration
 
-      // const skin=new pano2vrSkin(pano);
-
-
-			// create the panorama player with the container
-			const pano=new pano2vrPlayer("container");
-			// add the skin object
-			const skin=new pano2vrSkin(pano);
-			// load the configuration
-		
-			window.addEventListener("load", function() {
-				pano.readConfigUrlAsync("../output/pano.xml");
-			});
-
-
+window.addEventListener("load", function() {
+  pano.readConfigUrlAsync("../output/pano.xml");
+});
 
 // Write Javascript code!
 // const appDiv = document.getElementById('app');
